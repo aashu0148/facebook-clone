f3d1dee620c9324b35e3a1b0b8885333941d72ad
Nitish0007 <0007nitishsharma@gmail.com>
2021-02-08 21:06:21 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Navbar from './components/Navbar'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App">      
         <h1>It is working !</h1>
 
-        <Navbar/>
+        <Navbar />
         </div>
     )
+  }
 }
 
-export default App
+export default App;
