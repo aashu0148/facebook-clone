0bee85dbf06b7fe43616330156a1089e55a98bb0
unknown <dineshsoni8569@gmail.com>
2021-02-09 19:05:17 +0530

    SidebarRow

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
+        {/* <Navbar /> */}
+        <Sidebar />
       </div>
     );
   }
