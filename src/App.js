9ccf7c45d17a90733b9c7c24c0d8e0488dfb21a2
unknown <dineshsoni8569@gmail.com>
2021-02-10 21:17:55 +0530

    3rd try

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Feed from "./components/Feed";
+import Sidebar from "./components/Sidebar";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar optionActive="home" />
-        <div className="App_body">
+        {/* <Navbar /> */}
         <Sidebar />
-        <Feed />
-        </div>
       </div>
     );
   }
