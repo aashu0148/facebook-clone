2affc7104d3925eec5e1ab19dbf934bb14e7dd2c
aashu0148 <buildforfb@gmail.com>
2021-02-11 21:31:51 -0800

    Post Component made dynamic

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDPVBY66YCf21Vy41ATQL5Ih9ID4ji1zcQ",
-    authDomain: "clone01fb-15b59.firebaseapp.com",
-    projectId: "clone01fb-15b59",
-    storageBucket: "clone01fb-15b59.appspot.com",
-    messagingSenderId: "145806539167",
-    appId: "1:145806539167:web:de6f30def527491a0925aa",
-    measurementId: "G-HH6G1F48LF"
+  apiKey: "AIzaSyDPVBY66YCf21Vy41ATQL5Ih9ID4ji1zcQ",
+  authDomain: "clone01fb-15b59.firebaseapp.com",
+  projectId: "clone01fb-15b59",
+  storageBucket: "clone01fb-15b59.appspot.com",
+  messagingSenderId: "145806539167",
+  appId: "1:145806539167:web:de6f30def527491a0925aa",
+  measurementId: "G-HH6G1F48LF",
 };
