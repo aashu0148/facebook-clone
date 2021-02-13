29b1b6b509c1e1f216bfafcdb7745315546dc189
Nitish0007 <0007nitishsharma@gmail.com>
2021-02-13 11:28:32 +0530

    scrollbar fixed

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -6,8 +6,8 @@ function Widgets() {
     <div className="widgets">
       <div className="Widgets_sponsered">
         <iframe
-          src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fhellocoder&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-          width="340"
+          src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fhellocoder&tabs=timeline&width=302&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+          width="302"
           height="1000"
           style={{ border: "none", overflow: "hidden" }}
           scrolling="no"
